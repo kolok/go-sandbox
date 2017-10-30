1523a2e75b6795a86c2228d5f1e9f67fb490d795
Nicolas Oudard <noudard@weborama.com>
2017-10-30 10:10:40 +0100

    Manage 10 worker in parallel : Thank you @GabrielGlachant :)

--- main.go
+++ main.go
@@ -1,39 +1,85 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"sync/atomic"
+	"syscall"
 	"time"
 )
 
-var sem = make(chan int, 10)
-var count = make(chan int, 1)
+var count int32
 
-func handle(r int) {
-	sem <- 1  // Wait for active queue to drain.
-	worker(r) // May take a long time.
-	<-sem     // Done; enable next request to run.
+func generateData(ctx context.Context, queue chan<- int, maxData int) {
+	for i := 0; i < maxData; i++ {
+		select {
+		case <-ctx.Done():
+			fmt.Println("generateData stopping...")
+			close(queue)
+			return
+		default:
+			queue <- i
+		}
+	}
+	close(queue)
 }
 
-func worker(i int) {
-	fmt.Println("working with", i, "...")
-	time.Sleep(time.Second * time.Duration(i))
-	fmt.Println("done", i)
-	tmp := <-count
-	count <- tmp + 1
+func worker(ctx context.Context, i int, queue <-chan int) {
+	for value := range queue {
+		select {
+		case <-ctx.Done():
+			fmt.Println("worker ", i, "stopping...")
+			return
+		default:
+			fmt.Println("worker", i, "working with", value, "...")
+			time.Sleep(1 * time.Second)
+			fmt.Println("done", value)
+			atomic.AddInt32(&count, 1)
+		}
+	}
+}
+
+type workerFunc func(ctx context.Context, id int, queue <-chan int)
+
+func runWorkers(ctx context.Context, queue <-chan int, numWorkers int, worker workerFunc) {
+	var wg sync.WaitGroup
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			worker(ctx, i, queue)
+		}(i)
+	}
+	wg.Wait()
 }
 
 func main() {
-	queue := make(chan int, 20)
-	for i := 0; i < 20; i++ {
-		queue <- i
-	}
-	count <- 0
-	for {
-		req := <-queue
-		go handle(req) // Don't wait for handle to finish.
+	ctx, cancelFunc := context.WithCancel(context.Background())
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGTERM, syscall.SIGINT)
 
-		//		if tmp := <-count; tmp == 20 {
-		//			break
-		//		}
-	}
+	go func() {
+		var sigCount int
+		for sig := range sigChan {
+			sigCount++
+			switch sigCount {
+			case 1:
+				fmt.Printf("First signal %+v received. Cancelling...\n", sig)
+				cancelFunc()
+			default:
+				fmt.Printf("Another signal %+v received. Stopping...\n", sig)
+				os.Exit(1)
+			}
+		}
+	}()
+
+	queue := make(chan int, 20)
+	go generateData(ctx, queue, 100)
+
+	runWorkers(ctx, queue, 10, worker)
+
+	fmt.Println(count)
 }
